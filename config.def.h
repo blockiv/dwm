9cef70d33549d8087b366f0c0068004f481a5be7
blockiv <blockiv@tuta.io>
2023-02-19 23:04:31 -0500

    Apply underlinetags patch

--- config.def.h
+++ config.def.h
@@ -19,8 +19,13 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *alttags[] = { "<01>", "<02>", "<03>", "<04>", "<05>" };
+static const char *tags[] = { "cmd", "www", "crypto", "vm", "dev", "chat", "media" };
+static const char *alttags[] = { "[cmd]", "[www]", "[crypto]", "[vm]", "[dev]", "[chat]", "[media]" };
+
+static const unsigned int ulinepad	= 5;	/* horizontal padding between the underline and tag */
+static const unsigned int ulinestroke	= 2;	/* thickness / height of the underline */
+static const unsigned int ulinevoffset	= 0;	/* how far above the bottom of the bar the line should appear */
+static const int ulineall 		= 0;	/* 1 to show underline on all tags, 0 for just the active ones */
 
 static const Rule rules[] = {
 	/* xprop(1):
